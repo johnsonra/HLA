7a01b1821c5d369feef6303b6097cc9b17558c7c
Randy Johnson <johnsonra@mail.nih.gov>
2015-02-02 16:06:52 -0500

    Added HLA-DRB1 and HLA-DQA

--- hla.R
+++ hla.R
@@ -3,11 +3,13 @@
 # Randall Johnson
 # CCR Collaborative Bioinformatics Resource
 # Advanced Biomedical Computing Center at Frederick National Laboratory
+# Created January 30, 2015
+# Last Modified February 2, 2015
 
 
-################
-# HLA Antigens #
-################
+########################
+# HLA Class I Antigens #
+########################
 
 # this section contains broad splits and associated antigens primarily detectable by serology
 # see http://hla.alleles.org/antigens/broads_splits.html for details
@@ -16,15 +18,42 @@ tmp <- read.csv('data/hlaA_broad.csv', comment.char = '#', colClasses = 'charact
 hlaA.broad <- tmp$broadSpecificity
 names(hlaA.broad) <- tmp$alleleGroup
 
+save(hlaA.broad, file = 'HLA/data/hlaA_broad.RData')
+
 
 tmp <- read.csv('data/hlaB_broad.csv', comment.char = '#', colClasses = 'character')
 hlaB.broad <- tmp$broadSpecificity
 names(hlaB.broad) <- tmp$alleleGroup
 
+save(hlaB.broad, file = 'HLA/data/hlaB_broad.RData')
+
 
 tmp <- read.csv('data/hlaC_broad.csv', comment.char = '#', colClasses = 'character')
 hlaC.broad <- tmp$broadSpecificity
 names(hlaC.broad) <- tmp$alleleGroup
+
+save(hlaC.broad, file = 'HLA/data/hlaC_broad.RData')
+
+
+#########################
+# HLA Class II Antigens #
+#########################
+
+# As with the previous section, this section contains broad splits and associated antigens
+# see http://hla.alleles.org/antigens/broads_splits.html for details
+
+tmp <- read.csv('data/hlaDRB1_broad.csv', comment.char = '#', colClasses = 'character')
+hlaDRB1.broad <- tmp$broadSpecificity
+names(hlaDRB1.broad) <- tmp$alleleGroup
+
+save(hlaDRB1.broad, file = 'HLA/data/hlaDRB1_broad.RData')
+
+
+tmp <- read.csv('data/hlaDQA_broad.csv', comment.char = '#', colClasses = 'character')
+hlaDQA.broad <- tmp$broadSpecificity
+names(hlaDQA.broad) <- tmp$alleleGroup
+
+save(hlaDQA.broad, file = 'HLA/data/hlaDQA_broad.RData')
 
 
 ################
@@ -38,6 +67,8 @@ names(hlaC.broad) <- tmp$alleleGroup
 hlaBw <- read.csv('data/hlaBw.csv', comment.char = '#', stringsAsFactors = FALSE,
                   na.strings = '')
 
+save(hlaBw, file = 'HLA/data/hlaBw.RData')
+
 
 ################
 # HLA C Groups #
@@ -48,3 +79,5 @@ hlaBw <- read.csv('data/hlaBw.csv', comment.char = '#', stringsAsFactors = FALSE
 
 hlaCgrp <- read.csv('data/hlaCgrp.csv', comment.char = '#', stringsAsFactors = FALSE,
                     na.strings = '')
+
+save(hlaCgrp, file = 'HLA/data/hlaCgrp.RData')
